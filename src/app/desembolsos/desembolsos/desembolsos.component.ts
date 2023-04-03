f1dda633ef96566d6f2fda5f4cf573a5f1659568
Reikdns <reik.dns@gmail.com>
2023-04-03 06:16:41 -0500

    Prueba completa.

--- src/app/desembolsos/desembolsos/desembolsos.component.ts
+++ src/app/desembolsos/desembolsos/desembolsos.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ScriptService } from 'src/app/Services/scriptService/script.service';
+import { Data } from 'src/app/data/data';
+import { FiltroDesembolso } from 'src/app/interfaces/filtroDesembolso';
+import { OnInit } from '@angular/core'
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-desembolsos',
@@ -6,5 +12,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./desembolsos.component.css']
 })
 export class DesembolsosComponent {
+
+  data: Data;
+
+  tipoDeDocumento: string;
+  numeroDocumento: string;
+  numeroDeDesembolso: string;
+  fechaInicio: Date; 
+  fechaFinal: Date;
+  
+
+  constructor(private _scriptService: ScriptService)
+  {
+    this.data = new Data();
+    this.tipoDeDocumento = "";
+    this.numeroDeDesembolso = "";
+    this.numeroDocumento = "";
+    this.fechaFinal = new Date("08/09/9999 23:59:59");
+    this.fechaInicio = new Date("08/09/1 00:0:00");  
+  }
+
+  ngOnInit()
+  {
+    this._scriptService.loadScript("inputScript", "../../../assets/js/inputScript.js");
+  }
 
 }
